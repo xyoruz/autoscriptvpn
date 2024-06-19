1be02ec397e16b358a0a1f25a1db19830639a56b
XYR <169094507+xyoruz@users.noreply.github.com>
2024-06-20 06:02:42 +0800

    Add files via upload

---
+++ menu/clearcache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/pontora/autoscript-multiws/main/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+CEKEXPIRED
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+
+exit 0
+fi
+clear
+echo ""
+echo ""
+echo -e "[ \033[32mInfo\033[0m ] Clear RAM Cache"
+echo 1 > /proc/sys/vm/drop_caches
+sleep 3
+echo -e "[ \033[32mok\033[0m ] Cache cleared"
+echo ""
+echo "Back to menu in 2 sec "
+sleep 2
+menu
